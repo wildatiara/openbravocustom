806bffd1dbc46d005f6e60f27ab10910881fc3a5
thibaut.colson <thibaut.colson@59e4095a-f9fc-11de-8cdf-e97ad7d0f28a>
2010-02-03 09:27:02 +0000

    Ajout du montant en dette dans les listings de recherche client

--- OpenGreenCleanPos/src-pos/com/openbravo/pos/scripts/Derby-create.sql
+++ OpenGreenCleanPos/src-pos/com/openbravo/pos/scripts/Derby-create.sql
@@ -85,7 +85,7 @@ INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('14', 'Menu.Root', 0, $
 INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('15', 'Printer.CustomerPaid', 0, $FILE{/com/openbravo/pos/templates/Printer.CustomerPaid.xml});
 INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('16', 'Printer.CustomerPaid2', 0, $FILE{/com/openbravo/pos/templates/Printer.CustomerPaid2.xml});
 INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('17', 'payment.cash', 0, $FILE{/com/openbravo/pos/templates/payment.cash.txt});
-INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('18', 'banknote.100euro', 1, $FILE{/com/openbravo/pos/templates/banknote.100euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('31', 'banknote.100euro', 1, $FILE{/com/openbravo/pos/templates/banknote.100euro.png});
 INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('18', 'banknote.50euro', 1, $FILE{/com/openbravo/pos/templates/banknote.50euro.png});
 INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('19', 'banknote.20euro', 1, $FILE{/com/openbravo/pos/templates/banknote.20euro.png});
 INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('20', 'banknote.10euro', 1, $FILE{/com/openbravo/pos/templates/banknote.10euro.png});
